1f8c2f086609abc133f226489519ad32d50905bd
quyenphanlyons <quyenphanlyons@gmail.com>
2025-11-17 21:23:52 +0000

    edit1

---
+++ assignments/assignment02-bankholdiays.py
@@ -0,0 +1,4 @@
+# Program that prints out the dates of the bank holidays that happen in northern Ireland.
+# Author: Quyen
+
+# Modify the program to print the bank holidays that are unique to northern Ireland (i.e. do not happen elsewhere in the UK) you can choose if you want to use the name or the date of the holiday to decide if it is unique.
