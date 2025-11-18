56da961500e9431ce9048c2c37f7edaef308e60a
quyenphanlyons <quyenphanlyons@gmail.com>
2025-11-18 13:37:21 +0000

    edit2

--- assignments/assignment02-bankholdiays.py
+++ assignments/assignment02-bankholdiays.py
@@ -1,14 +1,15 @@
 # Program that prints out the dates of the bank holidays that happen in northern Ireland.
 # Author: Quyen
 
-# Modify the program to print the bank holidays that are unique to northern Ireland (i.e. do not happen elsewhere in the UK) you can choose if you want to use the name or the date of the holiday to decide if it is unique.
-
 import json
 
 filename = "bank-holidays.json"
 
-with open(filename,"r") as fp:
-    bh = json.load(fp)
+with open(filename,"r") as fn:
+    bh = json.load(fn)
     ni_bh = bh["northern-ireland"]["events"]
-print(ni_bh)
+    for row in ni_bh:
+       print (f"{row['date']}  {row['title']}")
 
+
+# Modify the program to print the bank holidays that are unique to northern Ireland (i.e. do not happen elsewhere in the UK) you can choose if you want to use the name or the date of the holiday to decide if it is unique.
